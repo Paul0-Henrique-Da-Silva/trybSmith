72dd13bf2632c752862db8675d8f1c2ac97c5e95
Paulo Henrique <ph46163835@gmail.com>
2022-10-06 15:48:08 -0300

    chore: modal products

---
+++ src/models/products.model.ts
@@ -0,0 +1,23 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IProducts } from '../interfaces/products.interfaces'
+import conn from './conn';
+
+export default class ModalProduct {
+  private conn: Pool;
+
+  constructor() {
+    this.conn = conn;
+  }
+
+  public create = async (products: IProducts): Promise<IProducts> => {
+    const { name, amount } = products;
+    const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?,?)';
+    const values = [name, amount];
+
+    const [result] = await this.conn.execute<ResultSetHeader>(query, values);
+    const { insertId: id } = result;
+
+    const created: IProducts = { id, ...products };
+    return created;
+  };
+}
