8f16265e98a75cf1287a140069db98d186df1302
Paulo Henrique <ph46163835@gmail.com>
2022-10-06 21:15:49 -0300

    feat: modal-service-controller de user e jwt

--- src/jwt/jwt.ts
+++ src/jwt/jwt.ts
@@ -14,34 +14,3 @@ const createdtoken = (user: IUsers): string => {
 };
 
 export = createdtoken;
-
-// import jwt, { Secret } from 'jsonwebtoken';
-// import dotenv from 'dotenv';
-// import { IUsers } from '../interfaces/users.interfaces';
-
-// dotenv.config();
-
-// const { JWT_SECRET } = process.env;
-// // const secret: string | object | Buffer = process.env.JWT_SECRET;
-
-// type Secret = string | Buffer | {
-//   key: string | Buffer;
-//   passphrase: JWT_SECRET;
-// }
-
-// const jwtConfig: jwt.SignOptions = {
-//   expiresIn: 3600,
-//   algorithm: 'HS256',
-// };
-
-// const createdtoken = (user: IUsers): string => {
-// //   const data = { id: user.id, username: user.username };
-
-//   const token = jwt.sign(
-
-//     { data: { username: user.username, id: user.id } }, Secret, jwtConfig);
-
-//   return token;
-// };
-
-// export = createdtoken;
