b33fa628428fe3f1f2e107c5b4969eb3fc124907
Paulo Henrique <ph46163835@gmail.com>
2022-10-07 00:31:13 -0300

    feat: add router em app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 import productsRouter from './routes/product.router';
 import userRouter from './routes/users.router';
+import orderRouter from './routes/orders.router';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productsRouter);
 app.use('/users', userRouter);
+app.use('/orders', orderRouter);
 
 export default app;
